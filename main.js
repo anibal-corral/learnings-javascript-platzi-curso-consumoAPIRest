f2738df3e0f09f6bbfe36efee5c61ab1aa821227
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-06-02 23:45:38 -0400

    Add favorites is eneable

--- main.js
+++ main.js
@@ -6,29 +6,61 @@ const spanError = document.getElementById("error");
 async function loadRandomCats(){
     const res =await fetch(API_URL_RANDOM);
     const data = await res.json();
-if(res.status!=200){
-    spanError.innerHTML="Error"
+    if(res.status!=200){
+        spanError.innerHTML="Error"
 }else
     {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
-    
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    btn1.onclick=()=>saveFavoriteCat(data[0].id);    
+    btn2.onclick=()=>saveFavoriteCat(data[1].id);    
     img1.src= data[0].url;
+    
     img2.src= data[1].url;
 }
     
 }
-async function favoritesCats(){
+async function loadFavoritesCats(){
     const res =await fetch(API_URL_FAVORITES);
     const data = await res.json();
     // console.log(data);
     if(res.status!=200){
         spanError.innerHTML=`Error: ${res.status} ${res.statusText}`
     }else{
-        
+        data.forEach((cat)=>
+        {
+            const section = document.getElementById("favoriteCats");
+            const article = document.createElement("article");
+            const img = document.createElement("img");
+            const btn = document.createElement("button");
+            const btnText = document.createTextNode("Delete Favorite Cat");
+
+            btn.appendChild(btnText);
+            img.src=cat.image.url;
+            img.width=200;
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article);
+        }
+        )
     }
-    
+}
+
+async function saveFavoriteCat(id){
+    const rest = await fetch(API_URL_FAVORITES,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({
+            image_id:id
+        })
+    });
+    // console.log('Saving rest', rest);
+
 }
 
 loadRandomCats();
-favoritesCats();
+loadFavoritesCats();
