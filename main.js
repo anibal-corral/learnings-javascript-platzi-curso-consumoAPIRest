5b4e5bca2396b03dd254db6d47e67a804926729f
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-06-02 23:13:27 -0400

    Structure prepared to get

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-const API_URL ="https://api.thecatapi.com/v1/images/search?api_key=88d8107a-c912-429a-b1f7-22658b889dba&limit=3";
+const API_URL_RANDOM ="https://api.thecatapi.com/v1/images/search?api_key=88d8107a-c912-429a-b1f7-22658b889dba&limit=2";
+const API_URL_FAVORITES="https://api.thecatapi.com/v1/favourites?api_key=88d8107a-c912-429a-b1f7-22658b889dba";
 
+const spanError = document.getElementById("error");
 
-
-async function reload(){
-    const res =await fetch(API_URL);
+async function loadRandomCats(){
+    const res =await fetch(API_URL_RANDOM);
     const data = await res.json();
-
+if(res.status!=200){
+    spanError.innerHTML="Error"
+}else
+    {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
+    
     img1.src= data[0].url;
     img2.src= data[1].url;
-    img3.src= data[2].url;
+}
+    
+}
+async function favoritesCats(){
+    const res =await fetch(API_URL_FAVORITES);
+    const data = await res.json();
+    // console.log(data);
+    if(res.status!=200){
+        spanError.innerHTML=`Error: ${res.status} ${res.statusText}`
+    }else{
+        
+    }
     
 }
 
-reload();
+loadRandomCats();
+favoritesCats();
