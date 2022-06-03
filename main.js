54c0c6d2e7695a831e5e3197da8720789a44cc97
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-06-02 22:25:30 -0400

    Using queryparameters

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const API_URL ="https://api.thecatapi.com/v1/images/search";
+const API_URL ="https://api.thecatapi.com/v1/images/search?limit=3";
 
 
 
 async function reload(){
     const res =await fetch(API_URL);
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src= data[0].url;
+
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    img1.src= data[0].url;
+    img2.src= data[1].url;
+    img3.src= data[2].url;
     
 }
 
